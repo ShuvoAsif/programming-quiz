70b5e96ef3e6aeb09eeba99ddc4f7c8e60613b5b
ShuvoAsif <asif.shuvo.learning@gmail.com>
2022-10-12 03:32:43 +0600

    chart

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { useLoaderData } from 'react-router-dom';
 import './Statistics.css';
 
 const Statistics = () => {
+    const data = useLoaderData().data;
+
     return (
-        <div>
-            <h1>this is statistics</h1>
+        <div className='chart'>
+            <BarChart
+                width={500}
+                height={300}
+                data={data}
+                margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="total" barSize={20} fill="#8884d8" />
+            </BarChart>
         </div>
     );
 };
