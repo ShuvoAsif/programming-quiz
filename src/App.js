bd73a4f4f14e4a7e0fdfd2e36a235ece54b8462d
ShuvoAsif <asif.shuvo.learning@gmail.com>
2022-10-11 22:06:20 +0600

    layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Main from './layouts/Main';
+import Home from './components/Home/Home';
+import Statistics from './components/Statistics/Statistics';
+import Blog from './components/Blog/Blog';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: 'statistics',
+          element: <Statistics></Statistics>
+        },
+        {
+          path: 'blog',
+          element: <Blog></Blog>
+        }
+      ]
+    },
+
+  ])
+
   return (
     <div className="App">
+      <RouterProvider router={router} ></RouterProvider>
     </div>
   );
 }
