42b1d74d77bf0eb43a8ea239362f5267c3d6ba60
ShuvoAsif <asif.shuvo.learning@gmail.com>
2022-10-12 04:04:01 +0600

    blog

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -3,8 +3,13 @@ import './Blog.css';
 
 const Blog = () => {
     return (
-        <div>
-            <h1>This is blog</h1>
+        <div className='blog'>
+            <h1>What is the use of react router?</h1>
+            <h3>React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL.React Router keeps your UI in sync with the URL. It has a simple API with powerful features like lazy code loading, dynamic route matching, and location transition handling built right in. </h3>
+            <h1>How does context API works?</h1>
+            <h3>React.createContext() is all you need. It returns a consumer and a provider. Provider is a component that as it's names suggests provides the state to its children. It will hold the "store" and be the parent of all the components that might need that store. Consumer as it so happens is a component that consumes and uses the state. More information can be found on React's documentation page.</h3>
+            <h1>What is the use of useRef hook?</h1>
+            <h3>The useRef Hook allows you to persist values between renders. It can be used to store a mutable value that does not cause a re-render when updated. It can be used to access a DOM element directly.</h3>
         </div>
     );
 };
