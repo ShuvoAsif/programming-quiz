fc4239568e67a9c111ab4a3ab527d99ee8404e21
ShuvoAsif <asif.shuvo.learning@gmail.com>
2022-10-12 20:59:56 +0600

    functions

--- src/components/Qustion/Qustion.js
+++ src/components/Qustion/Qustion.js
@@ -4,16 +4,30 @@ import './Qustion.css'
 import { faEye } from '@fortawesome/free-solid-svg-icons';
 
 const Qustion = ({ qustion }) => {
+    const ansHandeler = () => {
+        alert(qustion.correctAnswer);
+    }
+
+    const resultHandeler = (no) => {
+        if (qustion.options[no] === qustion.correctAnswer) {
+            alert('Your result is correct');
+        }
+        else {
+            alert('Your result is wrong');
+        }
+
+    }
+
     return (
         <div className='qustion'>
             <div className='qus'>
                 <h3>{qustion.question}</h3>
-                <FontAwesomeIcon icon={faEye}></FontAwesomeIcon>
+                <FontAwesomeIcon onClick={() => ansHandeler()} icon={faEye}></FontAwesomeIcon>
             </div>
-            <button>{qustion.options[0]}</button>
-            <button>{qustion.options[1]}</button>
-            <button>{qustion.options[2]}</button>
-            <button>{qustion.options[3]}</button>
+            <button onClick={() => resultHandeler(0)}>{qustion.options[0]}</button>
+            <button onClick={() => resultHandeler(1)}>{qustion.options[1]}</button>
+            <button onClick={() => resultHandeler(2)}>{qustion.options[2]}</button>
+            <button onClick={() => resultHandeler(3)}>{qustion.options[3]}</button>
         </div>
     );
 };
